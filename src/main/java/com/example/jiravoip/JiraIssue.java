2c28e47ec02f2301218853fb416facf1fadadc6a
Ilja Tataurovs <ilja.tataurov@gmail.com>
2025-04-11 19:21:34 +0300

    Backup restored JiraIssue and JiraService classes

--- src/main/java/com/example/jiravoip/JiraIssue.java
+++ src/main/java/com/example/jiravoip/JiraIssue.java
@@ -7,7 +7,40 @@ public class JiraIssue {
     private int callDuration;
     private String callTimestamp;
 
-    // Constructor
+    // Getters and setters
+    public String getCallerPhoneNumber() {
+        return callerPhoneNumber;
+    }
+
+    public void setCallerPhoneNumber(String callerPhoneNumber) {
+        this.callerPhoneNumber = callerPhoneNumber;
+    }
+
+    public String getRecipientPhoneNumber() {
+        return recipientPhoneNumber;
+    }
+
+    public void setRecipientPhoneNumber(String recipientPhoneNumber) {
+        this.recipientPhoneNumber = recipientPhoneNumber;
+    }
+
+    public int getCallDuration() {
+        return callDuration;
+    }
+
+    public void setCallDuration(int callDuration) {
+        this.callDuration = callDuration;
+    }
+
+    public String getCallTimestamp() {
+        return callTimestamp;
+    }
+
+    public void setCallTimestamp(String callTimestamp) {
+        this.callTimestamp = callTimestamp;
+    }
+
+    // Constructor for JiraIssue
     public JiraIssue(String callerPhoneNumber, String recipientPhoneNumber, int callDuration, String callTimestamp) {
         this.callerPhoneNumber = callerPhoneNumber;
         this.recipientPhoneNumber = recipientPhoneNumber;
@@ -15,37 +48,6 @@ public class JiraIssue {
         this.callTimestamp = callTimestamp;
     }
 
-    // Getters for all fields
-    public String getCallerPhoneNumber() {
-        return callerPhoneNumber;
-    }
-
-    public String getRecipientPhoneNumber() {
-        return recipientPhoneNumber;
-    }
-
-    public int getCallDuration() {
-        return callDuration;
-    }
-
-    public String getCallTimestamp() {
-        return callTimestamp;
-    }
-
-    // Optional Setters if you want to modify them later
-    public void setCallerPhoneNumber(String callerPhoneNumber) {
-        this.callerPhoneNumber = callerPhoneNumber;
-    }
-
-    public void setRecipientPhoneNumber(String recipientPhoneNumber) {
-        this.recipientPhoneNumber = recipientPhoneNumber;
-    }
-
-    public void setCallDuration(int callDuration) {
-        this.callDuration = callDuration;
-    }
-
-    public void setCallTimestamp(String callTimestamp) {
-        this.callTimestamp = callTimestamp;
-    }
+    // Default constructor
+    public JiraIssue() {}
 }
